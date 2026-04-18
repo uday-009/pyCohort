3ffb5e234183df28c44469779699d501f2410359
Uday Maroju <udaymaroju9@gmail.com>
2026-04-18 18:48:05 +0530

    input func

---
+++ inputFun.py
@@ -0,0 +1,7 @@
+a = int(input('enter a number: '));
+print(type(a)) # By default, the input function returns a string;
+
+b = int(input('enter first number: '));
+c = int(input('enter second number: '));
+d = b + c;
+print('the sum of the two numbers is: ', d)
